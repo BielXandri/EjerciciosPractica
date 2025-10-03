f1b38f6da32b73f7cc91c9ef26bac4381a095df3
BielXandri <bielxandri@gmail.com>
2025-10-03 19:10:36 +0200

    Quinta subida 40-45

--- exercises/P41_FromWhereToWhere.php
+++ exercises/P41_FromWhereToWhere.php
@@ -4,7 +4,15 @@ class P41_FromWhereToWhere
 {
     public function main(): void
     {
-        // Write your program here
-       
+        $from = (int) trim(fgets($GLOBALS['STDIN']  ??  STDIN));
+        $to = (int) trim(fgets($GLOBALS['STDIN']  ??  STDIN));
+
+        if ($from > $to) {
+            [$from, $to] = [$to, $from];
+        }
+
+        for ($i = $from; $i <= $to; $i++) {
+            echo $i . "\n"; 
+        }
     }
 }
--- exercises/P42_SumOfASequence.php
+++ exercises/P42_SumOfASequence.php
@@ -4,7 +4,19 @@ class P42_SumOfASequence
 {
     public function main(): void
     {
-        // Write your code here
        
+            $input = trim(fgets($GLOBALS['STDIN']  ??  STDIN));
+        $n = (int)$input; 
+
+        $sum = 0; 
+
+     
+        for ($i = 1; $i <= $n; $i++) {
+            $sum += $i;
+        }
+
+        echo "The sum is " . $sum . "\n";
     }
 }
+
+?>
